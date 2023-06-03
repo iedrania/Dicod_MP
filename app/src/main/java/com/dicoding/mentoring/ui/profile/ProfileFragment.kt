7a170d607cfd5f6828c8b8732a46594de291b389
farhanhariss <farhanharis66@gmail.com>
2023-06-03 16:53:14 +0700

    chore: add string, manifest, profile fragment

--- app/src/main/java/com/dicoding/mentoring/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/dicoding/mentoring/ui/profile/ProfileFragment.kt
@@ -47,14 +47,20 @@ class ProfileFragment : Fragment() {
             getUserDataProfile(token)
         }
 
+        //action when click Edit Profile Button
         binding.btnEditProfile.setOnClickListener{
             val intent = Intent(activity, ProfileActivity::class.java)
             activity?.startActivityForResult(intent,256)
         }
 
-        //action when click Add Interest
+        //action when click Edit Interest Button
         binding.btnEditInterest.setOnClickListener {
             val intent = Intent(activity, ListInterestActivity::class.java)
+            activity?.startActivity(intent)
+        }
+
+        binding.btnEditDays.setOnClickListener {
+            val intent = Intent(activity, ListDayActivity::class.java)
             activity?.startActivity(intent)
         }
 
