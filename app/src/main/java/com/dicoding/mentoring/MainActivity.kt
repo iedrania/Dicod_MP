714a2e982fd101295ed8dd29fedc25cf683b292c
farhanhariss <farhanharis66@gmail.com>
2023-06-06 16:33:41 +0700

    feat: differentiate navbar mentor & mentee

--- app/src/main/java/com/dicoding/mentoring/MainActivity.kt
+++ app/src/main/java/com/dicoding/mentoring/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.dicoding.mentoring.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,26 +19,44 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
         val navView: BottomNavigationView = binding.navView
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_bottom_navigation) as NavHostFragment
-        val navController = navHostFragment.navController
-//         val navController = findNavController(R.id.nav_host_fragment_activity_bottom_navigation)
+        val user = Firebase.auth.currentUser
+        user.let {
+            user?.getIdToken(false)?.addOnSuccessListener {
+                val claims = it.claims
 
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home,
-                R.id.navigation_message,
-                R.id.navigation_schedule,
-                R.id.navigation_profile
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+                // Role validation
+                val navGraphResId = if (claims["role"] == "mentor") {
+                    R.navigation.navigation_mentor
+                } else {
+                    R.navigation.navigation_mentee
+                }
+
+                val navHostFragment =
+                    supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_bottom_navigation) as NavHostFragment
+                val navController = navHostFragment.navController
+                val inflater = navController.navInflater
+                val graph = inflater.inflate(navGraphResId)
+
+                navController.graph = graph
+
+                // Passing each menu ID as a set of Ids because each
+                // menu should be considered as top level destinations.
+                val appBarConfiguration = AppBarConfiguration(
+                    setOf(
+                        R.id.navigation_home,
+                        R.id.navigation_message,
+                        R.id.navigation_schedule,
+                        R.id.navigation_profile
+                    )
+                )
+                setupActionBarWithNavController(navController, appBarConfiguration)
+                navView.setupWithNavController(navController)
+            }
+
+        }
+        setContentView(binding.root)
     }
+
 }
